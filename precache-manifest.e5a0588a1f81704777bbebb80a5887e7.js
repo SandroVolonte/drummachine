8cd75b9c846f75b1d2436d95bf80261b9884b4dd
SandroVolonte <61095194+SandroVolonte@users.noreply.github.com>
2020-07-23 14:43:40 +0200

    Updates

---
+++ precache-manifest.e5a0588a1f81704777bbebb80a5887e7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ef76ec2b5cc39a21db7d5b2da7ca71",
+    "url": "/drummachine/index.html"
+  },
+  {
+    "revision": "79f82d4f794d7b68e89b",
+    "url": "/drummachine/static/css/main.12f8b2e2.chunk.css"
+  },
+  {
+    "revision": "593739e3d3840039027b",
+    "url": "/drummachine/static/js/2.acb11fba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drummachine/static/js/2.acb11fba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79f82d4f794d7b68e89b",
+    "url": "/drummachine/static/js/main.f2a1c1e5.chunk.js"
+  },
+  {
+    "revision": "0ac050f1938f42e2b705",
+    "url": "/drummachine/static/js/runtime-main.ae9f8a70.js"
+  },
+  {
+    "revision": "22dcc3d938d6dc687f49a946484e87a9",
+    "url": "/drummachine/static/media/100_vocalize-the-groove.22dcc3d9.mp3"
+  },
+  {
+    "revision": "949a21708f97d19c2d8b3c53f2e77573",
+    "url": "/drummachine/static/media/80s-Bdrum1.949a2170.mp3"
+  },
+  {
+    "revision": "f0d445486f98c596c36def71c4d03075",
+    "url": "/drummachine/static/media/80s-COWBELL1.f0d44548.mp3"
+  },
+  {
+    "revision": "912c8db315878b35ee44bab502b36b15",
+    "url": "/drummachine/static/media/80s-CRASH1.912c8db3.mp3"
+  },
+  {
+    "revision": "406af532f3a519d11283414211795c53",
+    "url": "/drummachine/static/media/80s-TOM1.406af532.mp3"
+  },
+  {
+    "revision": "ccaca4cc9f51365391b5dd89aa9778ca",
+    "url": "/drummachine/static/media/80s-TOM2.ccaca4cc.mp3"
+  },
+  {
+    "revision": "17cc29c83d50291f84d1443aa026ac1b",
+    "url": "/drummachine/static/media/80s-TOM3.17cc29c8.mp3"
+  },
+  {
+    "revision": "81e60022fdf93b010f3523062dbfd72b",
+    "url": "/drummachine/static/media/80s-TOM5.81e60022.mp3"
+  },
+  {
+    "revision": "306dea7632f20846f7f0941d10851964",
+    "url": "/drummachine/static/media/female-harmony.306dea76.mp3"
+  }
+]);
